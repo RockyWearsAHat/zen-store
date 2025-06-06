5a168bf5d9e280d1b20d801fbd7a0f40e778274b
Rocky <alexwaldmann2004@gmail.com>
2025-06-06 17:48:12 -0500

    Hopefully access token and refresh token actually get saved for later use

--- server/db.ts
+++ server/db.ts
@@ -2,15 +2,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_LOCAL
-    ? ``
-    : `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.8rpqnmn.mongodb.net/balls?retryWrites=true&w=majority`,
-  {
-    retryWrites: true,
-    retryReads: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URI ?? "", {
+  retryWrites: true,
+  retryReads: true,
+});
 mongoose.connection.on("error", console.log.bind(console, "connection error:"));
 mongoose.connection.once("open", () => {
   console.log("Connected to Mongo");
